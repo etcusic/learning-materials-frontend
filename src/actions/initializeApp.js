e6c5b0d5f3c45f5fca1ea57248f205a25b7a4df0
Eli Cusic <tntitans31@gmail.com>
2021-05-11 13:20:10 -0500

    added API root checker

--- src/actions/initializeApp.js
+++ src/actions/initializeApp.js
@@ -1,6 +1,7 @@
 import { API_ROOT } from '../apiRoot'
 
 export const initializeApp = (setDecks) => {
+    console.log(API_ROOT)
     return fetch(`${API_ROOT}decks`)
     .then(response => response.json())
     .then(decks => {
