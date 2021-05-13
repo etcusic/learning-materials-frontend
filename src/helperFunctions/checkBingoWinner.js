4fbce46e351a334f40288143135cd3673ddc8f7b
Eli Cusic <tntitans31@gmail.com>
2021-05-13 13:53:06 -0500

    added diagonal checkers to check bingo winner function

--- src/helperFunctions/checkBingoWinner.js
+++ src/helperFunctions/checkBingoWinner.js
@@ -1,8 +1,13 @@
 export const checkBingoWinner = (boardMatrix) => {
     let matrixColors = [...boardMatrix].map(row => row.map(cell => cell.props.color))
-    console.log(matrixColors)
-    checkRows(matrixColors)
-    checkColumns(matrixColors)
+    let checkers = [
+        checkRows(matrixColors),
+        checkColumns(matrixColors),
+        checkDiagonalOne(matrixColors),
+        checkDiagonalTwo(matrixColors)
+    ]
+    console.log(`array => ${checkers}`)
+    console.log(`T or F => ${checkers.find(check => check === true)}`)
 }
 
 const checkRows = matrixColors => {
@@ -10,13 +15,21 @@ const checkRows = matrixColors => {
     if (matrixColors.find(row => row.every(cell => cell === "green"))){
         answer = true
     } 
-    console.log(`check rows => ${answer}`)
     return answer
 }
 
 const checkColumns = matrixColors => {
     let matrix = [[], [], [], [], []]
     matrixColors.forEach( row => row.forEach((cell, i) => matrix[i].push(cell)))
-    let answer = checkRows(matrix)
-    console.log(`check columns => ${answer}`)
+    return checkRows(matrix)
+}
+
+const checkDiagonalOne = matrixColors => {
+    let matrix = [matrixColors[0][0], matrixColors[1][1], matrixColors[2][2], matrixColors[3][3], matrixColors[4][4]]
+    return matrix.every(cell => cell === "green")
+}
+
+const checkDiagonalTwo = matrixColors => {
+    let matrix = [matrixColors[0][4], matrixColors[1][3], matrixColors[2][2], matrixColors[3][1], matrixColors[4][0]]
+    return matrix.every(cell => cell === "green")
 }
