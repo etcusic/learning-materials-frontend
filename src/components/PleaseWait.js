5dcdff8a037adec708725508351776a07389775b
Eli Cusic <tntitans31@gmail.com>
2021-05-18 15:26:58 -0500

    added apology msg to wait page

--- src/components/PleaseWait.js
+++ src/components/PleaseWait.js
@@ -4,6 +4,7 @@ const PleaseWait = () => {
     return (
         <div>
             <h2>Please wait while the data is being fetched.</h2>
+            <h2>We formally apologize for how slow Heroku is.</h2>
         </div>
     );
 }
