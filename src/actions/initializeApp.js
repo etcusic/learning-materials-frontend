661b871e91e8f17dc44eabf9aa137fb945d4943b
Eli Cusic <tntitans31@gmail.com>
2021-05-19 11:37:41 -0500

    changed fetch decks url to match change on heroku app

--- src/actions/initializeApp.js
+++ src/actions/initializeApp.js
@@ -1,7 +1,7 @@
 import { API_ROOT } from '../apiRoot'
 
 export const fetchDecks = () => {
-    return fetch(`${API_ROOT}api_decks`)
+    return fetch(`${API_ROOT}api/decks`)
     .then(response => response.json())
     .then(decks => decks)
 }
